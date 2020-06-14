950d15249005ebd4babb0fafadbfc43748b882f6
Maryam Heydarian <maryamhdr76@gmail.com>
2020-06-14 04:46:21 +0430

    create project modules

---
+++ src/app/modules/purchase/purchase.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shr/shared.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SharedModule
+  ]
+})
+export class PurchaseModule { }
