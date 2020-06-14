c686a6a1874938480ce6415db62c452fe2e2c14e
Maryam Heydarian <maryamhdr76@gmail.com>
2020-06-14 05:06:42 +0430

    add lazy loading concept to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
+  { path: 'search', loadChildren: () => import('./modules/search/search.module').then(m => m.SearchModule) },
+  { path: 'purchase', loadChildren: () => import('./modules/purchase/purchase.module').then(m => m.PurchaseModule) },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
