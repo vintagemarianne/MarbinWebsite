54dc82cbb484867dc1a30f66043272f69edfc064
Maryam Heydarian <maryamhdr76@gmail.com>
2020-07-27 14:44:55 +0430

    implement profile page view

--- src/app/modules/profile/pages/profile/profile.page.ts
+++ src/app/modules/profile/pages/profile/profile.page.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss']
 })
 export class ProfilePage implements OnInit {
+  isEditing: boolean = false;
+  isBusy: boolean = true;
+  isChangePasswordModalOpen: boolean = false;
+  isModalBusy: boolean = false;
+
+  newPassword: string = '';
+  currentPassword: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    this.isBusy = true;
+
+    this.isBusy = false;
+    this.isEditing = !this.isEditing;
+  }
+
+  changePassword() {
+    if (!this.newPassword || !this.currentPassword) {
+      alert('Invalid information.');
+      return;
+    }
+
+    if (this.currentPassword === this.newPassword) {
+      alert('New password and current password are equal.');
+      return;
+    }
+
+    this.isModalBusy = true;
+
+    this.newPassword = '';
+    this.currentPassword = '';
+    this.isModalBusy = false;
+    this.isChangePasswordModalOpen = false;
   }
 
 }
